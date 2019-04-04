f194949d0f52d3e6b0e306d5338001477295c32d
Hieu Vo H <hieu.vo-fr@asiantech.vn>
2019-04-04 09:07:20 +0700

    struct team1

---
+++ src/test/example.test.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+
+const Calculator = require('./example'); // we can report a code coverage report
+
+const cal = new Calculator();
+
+describe('calculate', () => {
+  it('add', () => {
+    const result = cal.Sum(5, 2);
+    expect(result).equal(7);
+  });
+
+  it('substract', () => {
+    const result = cal.Difference(5, 2);
+    expect(result).equal(3);
+  });
+});
