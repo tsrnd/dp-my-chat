341f3078b5be8efafb050f9d1105acb47bf87bed
at-hieuvo <hieu.vo@asiantech.vn>
2019-04-08 09:14:45 +0700

    test

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export class Routes {
     public routes(app): void {
-        app.route('/')
+        app.route('/'
             .get((req: Request, res: Response) => {
                 res.render('index');
             });
