f1fbb569979e4c9153bcdc232368f1661753b85c
an nguyen q <an.nguyen@asiantech.vn>
2019-04-11 17:05:18 +0700

    fix comment

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,13 +1,12 @@
 import * as mongoose from 'mongoose';
 import { Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
-import * as  fs from 'fs'; 
+
 
 export class HomeController {
     public index(req: Request, res: Response, next: any) {
         let token = req.cookies.token;
-        var userDecoded: any= jwt.decode(token, {complete: true});
-        console.log(userDecoded.payload);
+        let userDecoded: any = jwt.decode(token, {complete: true});
         res.render('index', {'userLogin' : userDecoded.payload});
     }
 }
