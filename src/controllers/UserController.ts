4fe480d224171baad7fe93ef6526dfe796218e44
ml-tam-nguyen4 <tam.nguyen4-fr@asiantech.vn>
2019-04-18 09:29:34 +0700

    api list user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import * as mongoose from 'mongoose';
 import { Request, Response } from 'express';
 import User from '../models/User';
 import { Md5 } from 'md5-typescript';
+import * as jwt from 'jsonwebtoken';
 
 const user = mongoose.model('users', User);
 
@@ -32,5 +33,17 @@ export class UserController {
                 }
             );
         });
+    }
+    public list(req: Request, res: Response, next: any) {
+        let token = req.cookies.token;
+        let userDecoded: any = jwt.decode(token, { complete: true });
+        var condition = { $or: [{ nickname: { $regex: '.*' + req.query.q + '.*' } }, { username: { $regex: '.*' + req.query.q + '.*' } }] };
+        user.find({ $and: [{ id: { $ne: userDecoded.payload.id } }, condition] }, (err, users) => {
+            if (err) {
+                return res.status(500).end();
+            }
+            return res.json(users);
+        });
+
     }
 }
