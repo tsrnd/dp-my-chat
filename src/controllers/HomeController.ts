90b7bd76b48b2689b0bd1c2a4fd1876ce39c8a02
ml-tam-nguyen4 <tam.nguyen4-fr@asiantech.vn>
2019-04-18 09:30:13 +0700

    update render index

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -2,13 +2,15 @@ import * as mongoose from 'mongoose';
 import { Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 import { User } from '../models/User';
+import { Room } from '../models/Room';
 
 const userModel = mongoose.model('User', User);
+const roomModel = mongoose.model('Room', Room);
 
 export class HomeController {
     public index(req: Request, resp: Response, next: any) {
         let token = req.cookies.token;
-        let userDecoded: any = jwt.decode(token, {complete: true});
+        let userDecoded: any = jwt.decode(token, { complete: true });
         userModel.findOne({ 'id': userDecoded.payload.id }, (err, user) => {
             if (err) {
                 return resp.status(500).end();
@@ -16,8 +18,12 @@ export class HomeController {
             if (!user) {
                 return resp.status(404).end();
             }
-
-            resp.render('index', {'userLogin' : user});
+            roomModel.find({}, (err, rooms) => {
+                if (err) {
+                    return resp.status(500).end();
+                }
+                resp.render('index', { 'userLogin': user, 'rooms': rooms });
+            });
         });
     }
 }
